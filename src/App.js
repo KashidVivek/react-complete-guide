be62764a0708396c97b6f66e765377119cacc670
KashidVivek <vivek.kashid@viit.ac.in>
2020-07-24 16:12:41 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sample React App!</h1>
+      <Person name='Vivek' age='23'/>
+      <Person name='Max' age='28'> Hobbies:Reading</Person>
     </div>
   );
 }
